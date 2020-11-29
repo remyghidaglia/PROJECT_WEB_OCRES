1a8487c8c762a6954f5253ed5d1f17a791bbf7bd
Rémy <remycaousou@gmail.com>
2020-11-29 19:47:25 +0100

    Tous les widgets sur la même page

--- frontend/app-frontend/src/App.js
+++ frontend/app-frontend/src/App.js
@@ -1,11 +1,6 @@
 import React from "react";
 import img1 from './img/info.png';
 import img2 from './img/piechart.png';
-import img3 from './img/world.png';
-import img4 from './img/money.png';
-import img5 from './img/barchart.png';
-import img6 from './img/cast.png';
-import img7 from './img/sales.png';
 import Formulaire from './Admin';
 import BarChart from './chart_100g.js';
 import Ecologie from './Ecologie';
@@ -22,54 +17,32 @@ import {
   Link
 } from "react-router-dom";
 
-// Each logical "route" has two components, one for
-// the sidebar and one for the main area. We want to
-// render both of them in different places when the
-// path matches the current URL.
 
-// We are going to use this route config in 2
-// spots: once for the sidebar and once in the main
-// content section. All routes are in the same
-// order they would appear in a <Switch>.
-const routes = [
+const WidgetsData = [
   {
-    path: "/",
-    exact: true,
-    title: () =><p>Bienvenue sur votre Dashboard</p>,
-    main: () => <Formulaire/>
-  },
-  {
-    path: "/BarChart",
-    title: () =><p>Voici la proportion des composants de votre produit (/100g)</p>,
-    main: () => (
-    <BarChart/>
-    )
+    title: <p>Voici la proportion des composants de votre produit (/100g)</p>,
+    main: <BarChart/>
 
   },
   {
-    path: "/Ecologie",
-    title: () =><p>Vous trouverez ici les indications sur l'impact écologique de votre produit.</p>,
-    main: () => <Ecologie/>
+    title: <p>Vous trouverez ici les indications sur l'impact écologique de votre produit.</p>,
+    main:  <Ecologie/>
   },
   {
-    path:"/Tags",
-    title: () =><p>Voici les différents tags en rapport avec votre produit</p>,
-    main: () => <Tags/>
+    title: <p>Voici les différents tags en rapport avec votre produit</p>,
+    main:  <Tags/>
   },
   {
-    path:"/Boire",
-    title: () =><p>Voici le score nutritif de votre aliment</p>,
-    main: () => <Score/>
+    title: <p>Voici le score nutritif de votre aliment</p>,
+    main:  <Score/>
   },
   {
-    path:"/Allergens",
-    title: () =><p>Voici un graphique présentant les potentiels allergènes présents dans votre produit.</p>,
-    main: () => <Allergens/>
+    title: <p>Voici un graphique présentant les potentiels allergènes présents dans votre produit.</p>,
+    main:  <Allergens/>
   },
   {
-    path:"/Ventes",
-    title: () =><p>Voici vos ventes au cours de l'année pour le nutella</p>,
-    main: () => <Ventes/>
+    title: <p>Voici vos ventes au cours de l'année pour le nutella</p>,
+    main:  <Ventes/>
   }
 ];
 
@@ -77,37 +50,38 @@ export default function SidebarExample() {
   return (
     <Router>
       <div id="menu-bar">
+        <div>
               <div className="button"><Link to="/"><img src={img1} alt="img1"></img></Link></div>
-              <div className="button"><Link to="/BarChart"><img src={img2} alt="img2"></img></Link></div>
-              <div className="button"><Link to="/Ecologie"><img src={img3}alt="img3"></img></Link></div>
-              <div className="button"><Link to="/Tags"><img src={img4}alt="img4"></img></Link></div>
-              <div className="button"><Link to="/Boire"><img src={img5}alt="img5"></img></Link></div>
-              <div className="button"><Link to="/Allergens"><img src={img6}alt="img6"></img></Link></div>
-              <div className="button"><Link to="/Ventes"><img src={img7}alt="img6"></img></Link></div>
+              <div className="button"><Link to="/Widgets"><img src={img2} alt="img2"></img></Link></div>
         </div>
+      </div>
 
         <div id="container">
           <Switch>
-            {routes.map((route, index) => (
-              <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              children={<route.title/>}
+            <Route
+              path={"/"}
+              exact={true}
+              children={<Formulaire/>}
             />
-            ))}
-          </Switch>
-          <Switch>
-            {routes.map((route, index) => (
-              <Route
-              key={index}
-              path={route.path}
-              exact={route.exact}
-              children={<route.main/>}
+            <Route
+              path={"/Widgets"}
+              exact={true}
+              children={<Widgets data={WidgetsData}/>}
             />
-            ))}
           </Switch>
         </div>
     </Router>
   );
+}
+
+const Widgets = ({data}) =>{
+  return (
+  <>
+    {data.map(w => (
+      <>
+      {w.title} 
+      {w.main}
+      </>
+    ))}
+  </>)
 }
